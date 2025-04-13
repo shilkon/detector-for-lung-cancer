f771397c93dc34cdfdc6a956c2bb049d29cf72e9
Bendryshev Sergey <bendryshev.sa@yandex.ru>
2025-04-13 12:31:32 +0300

    bash scripts init

---
+++ start_training.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Введите количество эпох для эксперимента: " experiment_epochs
+
+if [ -z "$experiment_epochs" ]; then
+    echo "Вы не ввели количество эпох. Завершение скрипта."
+    exit 1
+fi
+
+python3 -c "from run import run; run('training.LunaTrainingApp', '--epochs', '$experiment_epochs')"
